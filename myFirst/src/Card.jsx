4cd1b1a7992d451d1f1cf209c4da908cffd38fad
urmii15 <urmilasoni6363@gmail.com>
2026-04-06 15:02:18 +0530

    card

--- myFirst/src/Card.jsx
+++ myFirst/src/Card.jsx
@@ -1,13 +1,15 @@
-import medusa from ".assets/medusa.jpg"
+import medusa from "./assets/medusa.jpg"
 
 function Card() {
     return(
+        <>
        <div className="card">
             <img src="assets/medusa.jpg"></img>
             <h2>Bro Code</h2>
             <p>I make Codes and play videogames</p>
-       </div>>
-    )
+       </div>
+       </>
+    );
 }
 
-export default Card
+export default Card()
